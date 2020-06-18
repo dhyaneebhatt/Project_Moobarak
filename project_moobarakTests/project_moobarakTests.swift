20a36af25137bdc37da6397da5e5cb45a5b480e1
Alay Desai <alaydesai094@gmail.com>
2020-06-18 12:20:36 -0400

    Initial Commit

---
+++ project_moobarakTests/project_moobarakTests.swift
@@ -0,0 +1,34 @@
+//
+//  project_moobarakTests.swift
+//  project_moobarakTests
+//
+//  Created by Alay Desai on 2020-06-18.
+//  Copyright © 2020 Alay Desai. All rights reserved.
+//
+
+import XCTest
+@testable import project_moobarak
+
+class project_moobarakTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
